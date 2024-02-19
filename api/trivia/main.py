c2f6b054718c0da5a6f8bbe2cba7cf63d0db4f55
Noah Witt <noah@noah-witt.com>
2024-02-19 07:39:39 -0700

    API Basic Setup

---
+++ api/trivia/main.py
@@ -0,0 +1,21 @@
+import logging
+
+from fastapi import FastAPI
+
+FORMAT = "%(levelname)s: %(message)s"
+logging.basicConfig(format=FORMAT, level=logging.DEBUG)
+LOG = logging.getLogger(__name__)
+
+app = FastAPI(title="sfpdlog api", prefix="/api")
+
+@app.on_event("startup")
+async def on_startup() -> None:
+    """Initialize services on startup."""
+    # import sfpdlog.auth.endpoints as auth_endpoints
+    # import sfpdlog.endpoints as main_endpoints
+    # import sfpdlog.score.endpoints as score_endpoints
+
+
+@app.get("/api")
+async def root():
+    return {"message": "Hello World"}
