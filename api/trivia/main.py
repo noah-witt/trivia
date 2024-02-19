f98f9ecd97c60550cead86a46c53aca59e5a4cb5
Noah Witt <noah@noah-witt.com>
2024-02-19 08:02:40 -0700

    Basic Setup

--- api/trivia/main.py
+++ api/trivia/main.py
@@ -6,7 +6,7 @@ FORMAT = "%(levelname)s: %(message)s"
 logging.basicConfig(format=FORMAT, level=logging.DEBUG)
 LOG = logging.getLogger(__name__)
 
-app = FastAPI(title="sfpdlog api", prefix="/api")
+app = FastAPI(title="trivia api", prefix="/api")
 
 @app.on_event("startup")
 async def on_startup() -> None:
