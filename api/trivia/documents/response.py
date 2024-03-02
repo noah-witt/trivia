c870259e502514278ee1648cc6da99a896106b87
Noah Witt <noah@noah-witt.com>
2024-03-02 15:45:27 -0600

    Store Score

--- api/trivia/documents/response.py
+++ api/trivia/documents/response.py
@@ -8,9 +8,17 @@ class ResponseDocument(Document):
     responses: list[int]
     email: str
     name: str
+    _score: int | None = None  # _score is used as an index to store the score
 
     class Settings:
-        indexes = [IndexModel("quiz", unique=False)]
+        indexes = [
+            IndexModel("quiz", unique=False),
+            IndexModel(
+                #
+                "_score",
+                partialFilterExpression={"_score": {"$exists": True}},
+            ),
+        ]
 
     @property
     def score(self) -> int:
@@ -18,4 +26,5 @@ class ResponseDocument(Document):
         for i, response in enumerate(self.responses):
             if response == self.quiz.questions[i].correct:
                 correct += 1
+        self._score = correct
         return correct
