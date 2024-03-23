738e487ec39d0ff3d1e8cede543f09bcf93637a5
Noah Witt <noah@noah-witt.com>
2024-03-22 19:13:13 -0500

    Update Style

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,6 @@ import PrimeVue from 'primevue/config'
 import App from './App.vue'
 import './registerServiceWorker'
 import router from './router'
-import 'primevue/resources/themes/aura-light-green/theme.css'
+import 'primevue/resources/themes/aura-light-blue/theme.css'
 
 createApp(App).use(router).use(PrimeVue).mount('#app')
